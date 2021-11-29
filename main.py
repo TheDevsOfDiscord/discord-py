80164f8e1aab297e0fb906c32731186454de4f4e
Phoenix <phoenex659@gmail.com>
2021-11-29 15:50:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
-token = os.getenv('token')
+token = os.getenv('TOKEN')
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix=commands.when_mentioned_or",", strip_after_prefix = True, intents = intents)
 
