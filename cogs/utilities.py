18c667a9d876257e82dbef1b40b39d3e115a4cc0
Phoenix <phoenex659@gmail.com>
2021-12-18 11:31:37 +0530

    Update utilities.py

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -23,40 +23,21 @@ class Utilities(commands.Cog):
 
 
   @commands.command(pass_context=True)
-      async def lyrics(self, ctx, *args):
-          track = " ".join(args)
-          user = ctx.author
-          for activity in user.activities:
-              if isinstance(activity, Spotify):
-                  track = activity.title + " by " + str(activity.artist).split(";")[0]
-          if track == "" or track == " " or track.isspace():
-              await ctx.reply(":question: What track to search? Correct format: `~lyrics [Query]`", delete_after=10)
-              return
-          else:
-              wait = await ctx.reply(f":mag: Please hold on, searching for `{track}`")
-              try:
-                  search_lyrics = LyricsScraper()
-                  try:
-                      search_lyrics.musixmatch_lyrics(query=track)
-                  except TimeoutError:
-                      search_lyrics.google_lyrics(query=track)
-                  except:
-                      search_lyrics.genius_lyrics(query=track,
-                                            api_key="API Key")
-                  embed = discord.Embed(title=search_lyrics.title, description="**" + search_lyrics.artist + "**",
-                                    colour=0xffae00)
-                  lyric = str(str(search_lyrics.lyrics).strip()).split("\n\n")
-                  for i in lyric:
-                      embed.add_field(name="​", value=i, inline=False)
-                  embed.set_footer(text="Source: " + search_lyrics.source)
-                  try:
-                      await wait.edit(embed=embed, content="")
-                  except:
-                      embed = discord.Embed(title=":x: Something went wrong, can't show lyrics. Click here. ",
-                                            url=search_lyrics.url, colour=0xffae00)
-                      await wait.edit(embed=embed, content="")
-              except:
-                  await wait.edit(content=":x: Something went wrong, can't show lyrics ")
+  async def lyrics(ctx, *, song):
+       track = song.replace(" ", "+")
+       wait = await ctx.reply(f":mag: Please hold on, searching for `{song}`")
+       r=requests.get(f'https://some-cool-api.herokuapp.com/lyrics/?lyrics={track}')#This is my API feel free to use it
+       try:
+           res=r.json()
+           title = res['title']
+           artist = res['artist']
+           lyrics = res['lyrics']
+           source = res['source']
+           embed = disnake.Embed(title=f"**{title}**", description=f"**{artist}**\n\n\n{lyrics}\n", color=bot_embed_color)
+           embed.set_footer(text=f"Source: {source}")
+           await wait.edit(embed=embed)
+       except:
+           await wait.edit(content=f"Couldn't find any lyrics for `{song}`. Please try giving a more detailed search.")
       
       
   @commands.command(aliases=['bitcoin'])
