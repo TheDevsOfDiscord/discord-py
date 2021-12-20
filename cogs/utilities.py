6ec9c05881c60fd292f6028104482d3847d3cd80
Lonely Guy <phoenex659@gmail.com>
2021-12-20 15:07:00 +0530

    Update utilities.py

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -163,6 +163,21 @@ class Utilities(commands.Cog):
     embed=discord.Embed(title=f"{res['title']}", description=f"{res['explanation']}", color=bot_embed_color)
     embed.set_image(url=f"{res['hdurl']}")
     await ctx.reply(embed=embed)
+    
+  @commands.command()
+  async def pypi(self, ctx, package):
+    r = requests.get(f"https://pypi.org/pypi/{package}/json")
+    try:
+        res = r.json()
+        author = res['info']['author']
+        embed = discord.Embed(title=res['info']['name'], url = f"https://pypi.org/project/{package}/", description=f"{res['info']['summary']}\n\n\n", color=bot_embed_color)
+        embed.set_author(name="PyPI", url="https://pypi.org/", icon_url="https://thumbs.dreamstime.com/b/python-icon-filled-python-icon-website-design-mobile-app-development-python-icon-filled-development-collection-155362649.jpg")
+        embed.add_field(name="More Details :- \n\n", value=res['info']['home_page'], inline=False)
+        embed.set_thumbnail(url=f"https://pypi.org/static/images/twitter.6fecba6f.jpg")
+        await ctx.reply(embed=embed)
+    except:
+        embed = discord.Embed(title="Package Not Found!", description="Please check the spelling of the package name and try again!", color=bot_embed_color)
+        await ctx.reply(embed=embed)
   
 def setup(bot):
   bot.add_cog(Utilities(bot))
